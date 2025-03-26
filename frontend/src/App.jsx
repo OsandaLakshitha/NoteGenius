163b37b63ee4386d8d0c08d68e24cc57291229c6
Sachintha Dilshan <sachinthadilshan17285@gmail.com>
2025-03-26 16:34:07 +0530

    folder search sort add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import HandwrittenNotes from './pages/HandwrittenNotes';
 import VoiceNotes from './pages/VoiceNotes';
 import StructuredText from './pages/StructuredText';
 import Folders from './pages/Folders';
+import NoteyLandingPage from './pages/new';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/voice" element={<VoiceNotes />} />
         <Route path="/structured" element={<StructuredText />} />
         <Route path="/folders" element={<Folders />} />
+        
       </Routes>
     </Router>
   );
